bf6b1ba38f6fcc9f1daa5865367a8898e94c14ca
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2025-05-19 17:29:35 -0500

    ver etiqueta controller

---
+++ controller/etiqueta/VerEtiquetas.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '../../../model/entity/Etiqueta.php';
+require_once __DIR__ . '../../../model/crud/etiqueta/VerEtiqueta.php';
+class VerEtiquetas
+{
+    public static function verEtiquetas(){
+        return VerEtiqueta::verEtiquetas();
+    }
+}
