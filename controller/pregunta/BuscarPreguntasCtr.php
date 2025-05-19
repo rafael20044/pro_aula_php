415b991f0909e4843b20ce6fc7498faad4bb8fb1
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2025-05-19 17:26:50 -0500

    buscar preguntas controller

---
+++ controller/pregunta/BuscarPreguntasCtr.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '../../../model/crud/pregunta/BuscarPreguntas.php';
+
+class BuscarPreguntasCtr
+{
+    public static function buscarPreguntasCtr(){
+        return BuscarPreguntas::buscar();
+    }
+}
