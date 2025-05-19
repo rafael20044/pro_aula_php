d5c33d9111735d84011686ccb0718f2a07b436d0
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2025-05-19 17:23:38 -0500

    crear respuesta controller

---
+++ controller/respuesta/crearRespuesta.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__ . '../../../model/crud/respuesta/CrearRespuesta.php';
+session_start();
+
+$params = [
+    ':usuario_id' => $_POST['usuario_id'],
+    ':pregunta_id' => $_POST['pregunta_id'],
+    ':contenido' => $_POST['contenido']
+];
+
+$id = $params[':pregunta_id'];
+
+$seCreo = CrearRespuesta::crear($params);
+
+if ($seCreo) {
+    $_SESSION['ok'] = 'Respuesta creada con exito';
+    header("location: ../../view/pregunta.php?id=$id");
+}else{
+    $_SESSION['error'] = 'Error al crear la respuesta';
+    header("location: ../../view/pregunta.php?id=$id");
+}
