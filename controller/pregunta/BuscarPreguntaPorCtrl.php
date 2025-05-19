6dca00ded17ee185f3a567b1d4ac08183ffc38b1
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2025-05-19 17:26:18 -0500

    buscar pregunta por id controller

---
+++ controller/pregunta/BuscarPreguntaPorCtrl.php
@@ -0,0 +1,16 @@
+<?php
+require_once __DIR__ . '../../../model/crud/pregunta/BuscarPreguntaPor.php';
+require_once __DIR__ . '../../../model/crud/respuesta/BuscarRespuestaDepregunta.php';
+
+class BuscarPreguntaPorCtrl
+{
+    public static function buscar($id){
+        $pregunta = BuscarPreguntaPor::buscarPor($id);
+        $respuestas = BuscarRespuestaDepregunta::buscar($id);
+        $datos = [
+            'pregunta' => $pregunta,
+            'respuestas' => $respuestas
+        ];
+        return $datos;
+    }
+}
