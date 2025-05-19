6e46bc7afccbfdf891be035ef37c7754c10cc9c4
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2025-05-19 17:24:40 -0500

    gestionar reaccion controller

---
+++ controller/reaccion/gestionarReaccion.php
@@ -0,0 +1,17 @@
+<?php
+require_once __DIR__ . '../../../model/crud/reaccion/GestionarReaccion.php';
+
+session_start();
+
+$datos = [
+    ':usuario_id'=> $_POST['usuario_id'],
+    ':respuesta_id' => $_POST['respuesta_id'],
+    ':tipo' => $_POST['tipo']
+];
+
+$seCreo = GestionarReaccion::gestionar($datos);
+if ($seCreo) {
+    header("Location: ../../view/pregunta.php?id=" . $_POST['pregunta_id']);
+}else{
+    header("Location: ../../view/pregunta.php?id=" . $_POST['pregunta_id']);
+}
